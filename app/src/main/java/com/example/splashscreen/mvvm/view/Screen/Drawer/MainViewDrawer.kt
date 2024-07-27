f8bfad310e930b9b9167ed1b57775fe2a3f3f0a4
Gustavo Santiago <gustavohenriquesantiago5@gmail.com>
2024-07-26 21:41:22 -0300

    menu lateral

---
+++ app/src/main/java/com/example/splashscreen/mvvm/view/Screen/Drawer/MainViewDrawer.kt
@@ -0,0 +1,34 @@
+package com.example.splashscreen.mvvm.view.Screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun MainViewDrawer(navController: NavController) {
+
+}
+
+@Composable
+fun DrawerHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 64.dp),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "Header", fontSize = 60.sp)
+    }
+}
+
+@Composable
+fun DrawerBody() {
+    
+}
